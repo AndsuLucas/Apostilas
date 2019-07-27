c2bb6b2938ca974ecaa0a8ed9b1b180b95c6c33e
Anderson Lucas <a.lucassilvadeoliveira@gmail.com>
2019-07-27 15:39:14 -0300

    adicionando

---
+++ cursoc#/if/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            
+            if (isMale)
+            {
+                Console.WriteLine("is Male");
+            }else
+            {
+                Console.WriteLine("i'snt Male");
+            }
+
+        }
+    }
+}
---
+++ cursoc#/metodos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Anderson");
+            int cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
+        }
+        static void SayHi(string name)
+        {
+            Console.WriteLine("Hello world com métodos! Senhor "+name);
+        }
+        //retornando um valor inteiro
+        static int cube( int num )
+        {
+            int result = num * num * num;
+            return result;
+
+        }
+    }
+}
